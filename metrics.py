315440182f6531334d006feed0914d0e669ab7b9
Nam-SW <nsw0311@gmail.com>
2021-10-26 14:55:15 +0900

    feat: 테스트를 위한 loss 함수

--- metrics.py
+++ metrics.py
@@ -2,8 +2,15 @@ import tensorflow as tf
 
 
 def loss(y, pred):
-    return tf.keras.losses.binary_crossentropy(y, pred)
+    pred = tf.nn.softmax(pred)
+    idx_except_last = tf.meshgrid(
+        *[tf.range(s) for s in pred.shape[:-1]], indexing="ij"
+    )
+    idx = tf.stack(idx_except_last + [y], axis=-1)
+    pred_ = tf.gather_nd(pred, idx)
 
+    loss = tf.math.log(pred_)
+    mask = tf.cast(tf.math.not_equal(y, 0), dtype=loss.dtype)
 
-def accuracy(y, pred):
-    return tf.keras.metrics.binary_accuracy(y, pred)
+    loss = -tf.reduce_sum(loss * mask, axis=-1)
+    return tf.reduce_sum(loss) / tf.reduce_sum(mask)
