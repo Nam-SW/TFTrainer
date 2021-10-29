76c1def24cebc899757db063781af37fedf24e84
Ean <45066060+Nam-SW@users.noreply.github.com>
2021-10-29 16:08:06 +0900

    feat: 버전 업데이트

--- setup.py
+++ setup.py
@@ -1,21 +1,25 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name="TFTrainer",
-    version="0.0.1",
+    version="0.0.3.2",
     description="tensorflow utility trainer.",
     author="Nam-SW",
     author_email="nsw0311@gmail.com",
-    url="https://github.com/Nam-SW",
-    license="Apache",
-    python_requires=">=3.6.8",
+    url="https://github.com/Nam-SW/TFTrainer.git",
+    project_urls={
+        "Bug Tracker": "https://github.com/Nam-SW/TFTrainer/issues",
+    },
+    # license="Apache",
+    python_requires=">=3.6",
     install_requires=[
         "tensorflow>=2.4.1",
         "tensorboard",
         "tqdm",
         "pytz",
     ],
-    packages=["src"],
+    package_dir={"": "src"},
+    packages=find_packages(where="src"),
     classifiers=[
         "Programming Language :: Python :: 3.6",
         "License :: OSI Approved :: Apache Software License",
